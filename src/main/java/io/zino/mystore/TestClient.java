bb7e4ddad8b90706aca3e5e545922dae9ef281e1
Seyed M. T. Otaghsara <mohammadtaheri304@gmail.com>
2018-05-16 12:59:57 +0430

    some modification done

--- src/main/java/io/zino/mystore/TestClient.java
+++ src/main/java/io/zino/mystore/TestClient.java
@@ -6,41 +6,76 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
 
 public class TestClient {
+	final static Logger logger = Logger.getLogger(TestClient.class);
 	public static void main(String[] args) {
+		runCliClient();
+		//test();
+	}
 
+	private static void runCliClient() {
+		Map<String, String> map = new HashMap<>();
 		TestClient client = new TestClient();
 		try {
 			client.startConnection("localhost", 12341);
+			long count=0;
 			while (true) {
-				String sendMessage = client.sendMessage("add "+Random()+" qecwvr"+Math.random());
-				//System.out.println("client recive :: " + sendMessage);
+				count++;
+				{
+					String key = Random() + "";
+					String value = "qecwvr" + Math.random();
+					String sendMessage = client.sendMessage("add " + key + " " + value);
+					System.out.println((!map.containsKey(key))+" client recive :: " + sendMessage);
+					map.put(key, value);
+				}
+
+				{
+					String key = Random() + "";
+					String sendMessage2 = client.sendMessage("get " + key);
+					System.out.println(map.get(key)+" client recive :: " + sendMessage2);
+				}
+
+				{
+					String key = Random() + "";
+					String value = "qdecwc" + Math.random();
+					map.replace(key, value);
+					String sendMessage3 = client.sendMessage("update " + key + " " + value);
+					System.out.println("client recive :: " + sendMessage3);
+				}
+
+				{
+					String key = Random() + "";
+					String sendMessage4 = client.sendMessage("delete " + Random());
+					System.out.println(map.containsKey(key)+" client recive :: " + sendMessage4);
+					map.remove(key);
+				}
 				
-				String sendMessage2 = client.sendMessage("get "+Random());
-				//System.out.println("client recive :: " + sendMessage2);
-				
-				String sendMessage3 = client.sendMessage("update "+Random()+" qdecwc"+Math.random());
-				//System.out.println("client recive :: " + sendMessage3);
-				
-				String sendMessage4 = client.sendMessage("delete "+Random());
-				//System.out.println("client recive :: " + sendMessage4);
+				if(count%1000==0){
+					try {
+						Thread.sleep(1000L);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
 			}
 			// client.stopConnection();
 		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 	}
 
-	private static int Random(){
-		return (int)(Math.random()*1000000)%100;
+	private static int Random() {
+		return (int) (Math.random() * 10000000) % 10000;
 	}
-	
+
 	private Socket clientSocket;
 	private PrintWriter out;
 	private BufferedReader in;
